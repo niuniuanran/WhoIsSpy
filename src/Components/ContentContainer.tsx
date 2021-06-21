7588bc292261a66783be38cf10161c585e038edb
niuniuanran <irene.anran.niu@gmail.com>
2021-06-21 12:04:33 +1200

    Add go-back button

--- src/Components/ContentContainer.tsx
+++ src/Components/ContentContainer.tsx
@@ -1,11 +1,13 @@
-import {Grid, Box} from "@material-ui/core"
+import {Grid, IconButton, Button} from "@material-ui/core"
 import { makeStyles } from '@material-ui/core/styles';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+
 const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1,
       textAlign: 'center',
       margin: 0,
-      padding: '3rem',
+      padding: '2rem',
       width: '100vw',
       height: '100vh',
       boxSizing: 'border-box',
@@ -23,17 +25,33 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 interface MainLayoutProp {
+    allowBack?: boolean
     children:JSX.Element
 }
 
-export default function ContentContainer({children}:MainLayoutProp){
+export default function ContentContainer({children, allowBack}:MainLayoutProp){
     const classes = useStyles();
+        return <div className={classes.root}>
+            {
+                allowBack?(
+                    <Grid container >
+                        <Grid item xs={9} md={6} className={classes.content}> 
+                            {children}
+                        </Grid>
+                    </Grid>):(
+                    <Grid container spacing={0} direction="column">
+                        <Grid item xs={1}>
+                        <Button aria-label="back" color="primary" variant="text" size="large" style={{fontWeight: "bolder"}}>
+                            <ArrowBackIosIcon /> Back
+                        </Button>
+                        </Grid>
+                        <Grid container item xs={9} md={6} className={classes.content}> 
+                            {children}
+                        </Grid>
+                    </Grid>
+                )
+            }
+        </div>
 
-    return <div className={classes.root}>
-        <Grid container >
-            <Grid item xs={9} md={6} className={classes.content}> 
-                {children}
-            </Grid>
-        </Grid>
-    </div>
+    
 }
