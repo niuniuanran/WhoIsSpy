f996486b1213301bf2c1d2368f9c5abab8efd798
niuniuanran <irene.anran.niu@gmail.com>
2021-06-25 22:54:24 +1200

    Enter room

--- src/Components/Room/Room.tsx
+++ src/Components/Room/Room.tsx
@@ -1,16 +1,18 @@
 import { useParams } from "react-router-dom";
-import {useContext} from "react"
+import { useContext } from "react"
+import { Paper } from "@material-ui/core"
 import { PlayerContext, PlayerContextType } from "../../Contexts/PlayerContext";
 import NameUserForRoom from "./NameUserForRoom";
+import ContentContainer from "../Shared/ContentContainer"
 
 export default function Room(){
     const { nickname } = useContext(PlayerContext) as PlayerContextType
     const { code } = useParams<{code?: string}>()
 
-    return <div>
-        {nickname? <div>
-            {code}
-        </div>:
-        <NameUserForRoom />}
-    </div>
+    if (!nickname) {
+        return <NameUserForRoom />
+    }
+    return <ContentContainer> 
+        <Paper>Code: {code}</Paper>
+    </ContentContainer>
 }
