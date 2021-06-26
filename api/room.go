b223abd73dfc0d8c040743d01002a3051e6b27d0
niuniuanran <irene.anran.niu@gmail.com>
2021-06-26 22:45:45 +1200

    Implement generate code

--- api/room.go
+++ api/room.go
@@ -1,14 +1,20 @@
 package main
 
+import "strconv"
+
+var roomCodeIncr = 1001
+
 type Room struct {
-	Code       string `json:"code"`
-	PlayerNum  int    `json:"playerNum"`
-	SpyNum     int    `json:"spyNum"`
-	Language   string `json:"language"`
-	players    map[*Player]bool
-	register   chan *Player
-	unregister chan *Player
-	broadcast  chan *BroadcastMessage
+	Code         string `json:"code"`
+	PlayerNum    int    `json:"playerNum"`
+	SpyNum       int    `json:"spyNum"`
+	Language     string `json:"language"`
+	EighteenPlus bool   `json:"eighteenPlus"`
+	RandomBlank  bool   `json:"randomBlank"`
+	players      map[*Player]bool
+	register     chan *Player
+	unregister   chan *Player
+	broadcast    chan *BroadcastMessage
 }
 
 type RoomSettings struct {
@@ -16,21 +22,25 @@ type RoomSettings struct {
 	PlayerNum    int    `json:"playerNum"`
 	SpyNum       int    `json:"spyNum"`
 	EighteenPlus bool   `json:"eighteenPlus"`
+	RandomBlank  bool   `json:"randomBlank"`
 }
 
 func NewRoom(roomSettings RoomSettings) *Room {
 	return &Room{
-		Code:       generateCode(),
-		Language:   roomSettings.Language,
-		PlayerNum:  roomSettings.PlayerNum,
-		SpyNum:     roomSettings.SpyNum,
-		players:    make(map[*Player]bool, roomSettings.PlayerNum),
-		register:   make(chan *Player),
-		unregister: make(chan *Player),
-		broadcast:  make(chan *BroadcastMessage),
+		Code:         generateCode(),
+		Language:     roomSettings.Language,
+		PlayerNum:    roomSettings.PlayerNum,
+		SpyNum:       roomSettings.SpyNum,
+		EighteenPlus: roomSettings.EighteenPlus,
+		RandomBlank:  roomSettings.RandomBlank,
+		players:      make(map[*Player]bool, roomSettings.PlayerNum),
+		register:     make(chan *Player),
+		unregister:   make(chan *Player),
+		broadcast:    make(chan *BroadcastMessage),
 	}
 }
 
 func generateCode() string {
-	return "1234"
+	roomCodeIncr++
+	return strconv.Itoa(roomCodeIncr)
 }
