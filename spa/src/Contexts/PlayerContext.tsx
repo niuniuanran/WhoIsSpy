0d1339e9247269649b86fc17f5ea899dc031d40d
niuniuanran <irene.anran.niu@gmail.com>
2021-06-27 22:40:39 +1200

    Use .env file for api uri

--- spa/src/Contexts/PlayerContext.tsx
+++ spa/src/Contexts/PlayerContext.tsx
@@ -28,7 +28,7 @@ function PlayerProvider({ children }: PlayerContextProp){
 
     useEffect(() => {
         if (nickname && code && !connected){
-            ws.current = new WebSocket(`ws://localhost:8080/ws?nickname=${nickname}&roomcode=${code}`)
+            ws.current = new WebSocket(`ws://${process.env.REACT_APP_API_BASE_URL}/ws?nickname=${nickname}&roomcode=${code}`)
             ws.current.onopen = () => {
                 // on connecting, do nothing but log it to the console
                 console.log("connected");
