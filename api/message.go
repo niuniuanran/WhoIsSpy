f9c9f9a095acd4c84cb297cce59f131685b40440
niuniuanran <irene.anran.niu@gmail.com>
2021-07-09 15:39:48 +1200

    Tidy up go files

--- api/message.go
+++ api/message.go
@@ -6,6 +6,40 @@ import (
 	"log"
 )
 
+type Message interface {
+	encode() []byte
+}
+
+const (
+	PlayerJoinedBroadcast   = "player-joined"
+	PlayerLeftBroadcast     = "player-left"
+	PlayerNewStateBroadcast = "player-state"
+	VoteResultBroadcast     = "vote-result"
+	GameWillStartBroadcast  = "game-will-start"
+	YourWordBroadcast       = "your-word"
+)
+
+type BroadcastMessage struct {
+	Action   string `json:"action"`
+	Payload  string `json:"payload"`
+	RoomCode string `json:"roomcode"`
+	Line     string `json:"line"`
+}
+
+func (message *BroadcastMessage) encode() []byte {
+	json, err := json.Marshal(message)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return json
+}
+
+func (message *BroadcastMessage) toString() string {
+	return fmt.Sprintf("Action: %s, Payload: %s",
+		message.Action, message.Payload)
+}
+
 const (
 	TalkFinishAction      = "talk-finish"
 	VoteAction            = "vote"
